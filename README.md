93f32832244c384893fb77aa3c3882b45d0b309b
santhosh <santhoshsanti329@gmail.com>
2024-09-25 01:24:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Build Rock, Paper and Scissors Game with React JS
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To run in hot module reloading mode:
+
+```sh
+npm start
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+## Runningg
+
+Open the file `dist/index.html` in your browser
+
+
---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+const Footer = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <footer className="footer">
+        <div class="attribution">
+          {" "}
+          
+          Coded by <a href="https://www.linkedin.com/in/santhoshkumar6" target="_blank">Santhosh Kumar</a>.
+        </div>
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
+};
+
+export default Footer;
